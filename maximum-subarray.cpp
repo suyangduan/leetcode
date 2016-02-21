5f56cca93baffb8815369e67f915fc7a390b17ad
suyangduan <d.o.frank@hotmail.com>
2016-02-21 11:29:21 -0800

    leetcode-53: add initial solution

---
+++ maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = nums[0];
+        int max_sum = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++)
+        {
+            sum = max(sum + nums[i], nums[i]);
+            max_sum = max(max_sum, sum);
+        }
+        
+        return max_sum;
+        
+    }
+};
